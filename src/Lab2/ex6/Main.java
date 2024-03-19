e543ebc5b11749bf5b20aefecdf4bc1600dbe5a2
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-03-19 23:35:57 +0200

    tema lab3 ex7

--- src/Lab2/ex6/Main.java
+++ src/Lab2/ex6/Main.java
@@ -2,19 +2,19 @@ package Lab2.ex6;
 
 public class Main {
     public static void main(String[] args) {
-        Patrulater patrulater=new Patrulater(5,6,7,8,90,90,90,90);
-
-        Paralelogram paralelogram=new Paralelogram(5,6,60,30);
-        System.out.println(paralelogram.arie());
-
-        Romb romb=new Romb(1,1,60,30,2,1);
-        System.out.println(romb.arie());
-
-        Dreptunghi dreptunghi=new Dreptunghi(10,5);
-        System.out.println(dreptunghi.arie());
-
-        Patrat patrat=new Patrat(8);
-        System.out.println(patrat.arie());
+//        Patrulater patrulater=new Patrulater(5,6,7,8,90,90,90,90);
+//
+//        Paralelogram paralelogram=new Paralelogram(5,6,60,30);
+//        System.out.println(paralelogram.arie());
+//
+//        Romb romb=new Romb(1,1,60,30,2,1);
+//        System.out.println(romb.arie());
+//
+//        Dreptunghi dreptunghi=new Dreptunghi(10,5);
+//        System.out.println(dreptunghi.arie());
+//
+//        Patrat patrat=new Patrat(8);
+//        System.out.println(patrat.arie());
     }
 
 }
--- src/Lab2/ex7/Main.java
+++ src/Lab2/ex7/Main.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class Main {
     public static void Main(String[] args) {
-        List<CalculatorResult> calculationResults = SmarterCalculator.calculate(args);
-
-        for (CalculatorResult result : calculationResults) {
-            CalculatorRequest request = result.getRequest();
-            System.out.println("Operation " + request + " has result " + result.computeResult());
-        }
+//        List<CalculatorResult> calculationResults = SmarterCalculator.calculate(args);
+//
+//        for (CalculatorResult result : calculationResults) {
+//            CalculatorRequest request = result.getRequest();
+//            System.out.println("Operation " + request + " has result " + result.computeResult());
+//        }
     }
 }
